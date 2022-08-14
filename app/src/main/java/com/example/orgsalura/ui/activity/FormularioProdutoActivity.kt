4b3615e95e95bddc802ec3e2d52eb9929f349b87
Rafael <rafapgbr@gmail.com>
2022-08-14 10:44:45 -0300

    refactor! extraindo métodos e melhorando a legibilidade do código.

--- app/src/main/java/com/example/orgsalura/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgsalura/ui/activity/FormularioProdutoActivity.kt
@@ -1,14 +1,13 @@
 package com.example.orgsalura.ui.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import com.example.orgsalura.R
 import com.example.orgsalura.ui.dao.ProdutosDao
 import com.example.orgsalura.ui.model.Produto
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 /**
@@ -22,41 +21,49 @@ import java.math.BigDecimal
  * conjunto de bibliotecas do "Android Jetpack", que é um conjunto de
  * bibliotecas bem vistas pela ocmunidade.
  */
-class FormularioProdutoActivity : AppCompatActivity() {
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) { //Estamos definindo o layout que será carregado.
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario_produto)
 
-        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        configurarBotaoSalvar()
+    }
+
+    private fun configurarBotaoSalvar() {
+
+        val botaoSalvar = findViewById<Button>(R.id.activity_formulario_produto_botao_salvar)
+
+        val dao = ProdutosDao()
 
         botaoSalvar.setOnClickListener { //Quando o botão "botao_salvar" for pressionado, os dados do campo serão enviados para essa variável.
-
-            val campoNome = findViewById<EditText>(R.id.nome)
-            val nome = campoNome.text.toString() //Estamos obtendo o texto que foi inserido no campo "nome".
-
-            val campoDescricao = findViewById<EditText>(R.id.descricao)
-            val descricao = campoDescricao.text.toString()
-
-            val campoPreco = findViewById<EditText>(R.id.valor)
-            val precoEmTexto = campoPreco.text.toString()
-
-            val valor = if(precoEmTexto.isBlank()){ //Estamos utilizando uma "if expression"
-                BigDecimal.ZERO
-            }else{
-                BigDecimal(precoEmTexto)
-            }
-
-            val produtoNovo = Produto(
-                nome = nome,
-                descricao = descricao,
-                valor = valor
-            )
-
-            val dao = ProdutosDao()
+            val produtoNovo = criaProduto()
             dao.adiciona(produtoNovo)
             finish() //Fecharemos essa Activity e retornaremos para a activity anterior da pilha de activities.
         }
+    }
+
+    private fun criaProduto(): Produto {
+
+        val campoNome = findViewById<EditText>(R.id.activity_formulario_produto_nome)
+        val nome = campoNome.text.toString() //Estamos obtendo o texto que foi inserido no campo "nome".
+
+        val campoDescricao = findViewById<EditText>(R.id.activity_formulario_produto_descricao)
+        val descricao = campoDescricao.text.toString()
+
+        val campoPreco = findViewById<EditText>(R.id.activity_formulario_produto_valor)
+        val precoEmTexto = campoPreco.text.toString()
+
+        val valor = if (precoEmTexto.isBlank()) { //Estamos utilizando uma "if expression" para atribuir o valor dos códigos no bloco do "if" diretamente à variável "valor".
+            BigDecimal.ZERO
+        } else {
+            BigDecimal(precoEmTexto)
+        }
+
+        return Produto( //Retornando o produto que foi criado com base nos dados do formulário.
+            nome = nome,
+            descricao = descricao,
+            valor = valor
+        )
     }
 }
